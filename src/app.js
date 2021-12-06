3b15e50498bd67c1bddd8b4d0a2d248b42e95153
jnguyen1995 <jon.j.nguyen1995@gmail.com>
2021-12-06 10:37:11 -0500

    Set up routing files, migrations, seeding, and basic error handlers

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
---
+++ src/errors/notFound.js
@@ -0,0 +1,8 @@
+/**
+ * Express API "Not found" handler.
+ */
+function notFound(req, res, next) {
+  next({ status: 404, message: `cannot be found: ${req.originalUrl}` });
+}
+
+module.exports = notFound;
